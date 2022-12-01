c3dddd0a41705c52020158e60b06f0cb8c516bcd
Nakul Bhardwaj <nakul.bhardwaj@intel.com>
2022-12-01 18:10:31 +0530

    Update setup.py
    commented out name and version for testing api

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     # name="edgesoftware",
-    version="1.0.1",
+    # version="1.0.1",
     description="A CLI wrapper for management of Intel® Edge Software Hub packages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
